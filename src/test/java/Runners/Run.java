af00a29633b4bead0495d00c107cd21502ba7757
Frajola <Nicholasmoreira18@gmail.com>
2022-09-01 23:30:51 -0300

    Second commit

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/firstTest.feature",
+        features = {"src/test/features/"},
         glue = {"StepsDefinitions"}
 )
 
